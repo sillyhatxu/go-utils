cf4b88a43812c797c8e05b342d088acbdf6e6479
shikuan.xu <shikuan.xu@advance.ai>
2019-06-18 14:52:09 +0800

    add response message

--- header/header.go
+++ header/header.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/language"
 )
 
 func GetDeviceId(context *gin.Context) string {
@@ -14,6 +15,44 @@ func GetDeviceId(context *gin.Context) string {
 		return values[0]
 	}
 	return ""
+}
+
+func GetAcceptLanguage(context *gin.Context) language.Tag {
+	local := language.English
+	acceptLanguage := context.Request.Header["Accept-Language"]
+	if acceptLanguage != nil && len(acceptLanguage) > 0 {
+		switch acceptLanguage[0] {
+		case "zh":
+			local = language.Chinese
+		case "id":
+			local = language.Indonesian
+		default:
+			//"en-US"
+			local = language.English
+		}
+	}
+	return local
+}
+
+type ResponseMessage struct {
+	Context *gin.Context
+	MsgZH   string
+	MsgEN   string
+	MsgID   string
+}
+
+func (rm ResponseMessage) GetResponseMessage() string {
+	tag := GetAcceptLanguage(rm.Context)
+	switch tag {
+	case language.Indonesian:
+		return rm.MsgID
+	case language.English:
+		return rm.MsgEN
+	case language.Chinese:
+		return rm.MsgZH
+	default:
+		return rm.MsgEN
+	}
 }
 
 func GetCurrentUserId(context *gin.Context, secretKey string, enable bool) (string, error) {
