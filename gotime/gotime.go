e68f53b6a6f3c9005cd7e92b29d5dd66b7ddd607
shikuan.xu <shikuan.xu@advance.ai>
2019-07-24 11:22:47 +0800

    fix to millisecond

--- gotime/gotime.go
+++ gotime/gotime.go
@@ -3,8 +3,9 @@ package gotime
 import "time"
 
 const (
-	SingaporeLocal = "Asia/Singapore"
-	LayoutTime     = "2006-01-02 15:04:05"
+	SingaporeLocal                  = "Asia/Singapore"
+	AsiaSingaporeHour time.Duration = 8
+	LayoutTime                      = "2006-01-02 15:04:05"
 )
 
 func Local(name string) (*time.Location, error) {
@@ -31,6 +32,12 @@ func FormatTimestamp(i int64) time.Time {
 	return time.Unix(0, i*int64(time.Millisecond))
 }
 
-func ToMillisecond(t time.Time) int64 {
-	return t.Local().UnixNano() / int64(time.Millisecond)
+func ToLocationMillisecond(t time.Time) int64 {
+	return TimeFormat(t).UnixNano() / int64(time.Millisecond)
+}
+
+func TimeFormat(inputTime time.Time) time.Time {
+	secondsEastOfUTC := int((AsiaSingaporeHour * time.Hour).Seconds())
+	timezoneAsiaSingapore := time.FixedZone(SingaporeLocal, secondsEastOfUTC)
+	return time.Date(inputTime.Year(), inputTime.Month(), inputTime.Day(), inputTime.Hour(), inputTime.Minute(), inputTime.Second(), inputTime.Nanosecond(), timezoneAsiaSingapore)
 }
